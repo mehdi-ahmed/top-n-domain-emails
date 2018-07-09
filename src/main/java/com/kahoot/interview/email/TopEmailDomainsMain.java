f4f00fe1c3225189cc59779de0a43194c7a21103
mehdi-ahmed <ahmed.mehdi.2009@gmail.com>
2018-07-09 23:25:58 +0200

    logging added

--- src/main/java/com/kahoot/interview/email/TopEmailDomainsMain.java
+++ src/main/java/com/kahoot/interview/email/TopEmailDomainsMain.java
@@ -8,24 +8,29 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.logging.Logger;
 
+import static com.kahoot.interview.email.util.EmailUtil.EMAILS_OUTPUT_CSV;
 import static com.kahoot.interview.email.util.EmailUtil.printMap;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 public class TopEmailDomainsMain {
 
+    private static final Logger LOGGER = Logger.getLogger(TopEmailDomainsMain.class.getName());
+
     public static void main(String[] args) throws IOException {
 
         //Step 1 : Generate a CSV file in RESOURCES folder containing 100 random Emails.
         List<String> emails = EmailUtil.generateRandomEmails();
         CsvWriter writer = new CsvWriter();
-        writer.writeCsv(emails, EmailUtil.EMAILS_OUTPUT_CSV);
+        writer.writeCsv(emails, EMAILS_OUTPUT_CSV);
+        LOGGER.info(String.format("CSV file located in %s", EMAILS_OUTPUT_CSV));
 
 
         //Step 2 : Read the CSV File, retrieve a list of domains and count frequency..
         CsvReader reader = new CsvReader();
-        List<String> domains = reader.readCsv(EmailUtil.EMAILS_OUTPUT_CSV);
+        List<String> domains = reader.readCsv(EMAILS_OUTPUT_CSV);
         Map<String, Long> domainsWithFrequencyMap = domains.stream().collect(groupingBy(Function.identity(), counting()));
 
 
